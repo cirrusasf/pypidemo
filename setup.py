ea1fcdf83ab5ff6fc7ea6acea92123f23cb228b0
jzhu4 <jzhu4@alaska.edu>
2021-04-22 16:12:58 -0800

    add download link to setup.py. 2021-04-22.

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
   author = 'Jiang Zhu',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  download_url = 'https://github.com/cirrusasf/pypidemo/archive/refs/tags/v0.1-alpha.tar.gz',    # I explain this later on
+  keywords = ['demo', 'pypi', 'packing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'netCDF4',
           'osgeo',
